30487591ade0364c3a6b736738790ea9f64eae34
Ricardo Filipe Marinho Braz <47540686+RicardoMarinho@users.noreply.github.com>
2019-06-20 11:29:11 +0100

    pasta config

---
+++ config/init.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+// estabelecer ligação com a base de dados
+$dsn = 'mysql:dbname=moodle;host=127.0.0.1';
+$user = 'root';
+$password = '';
+
+try {
+    $conn = new PDO($dsn, $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+?>
